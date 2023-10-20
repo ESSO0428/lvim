044b090bd03ee54876049b23f545b5c2b84b2e33
Andy6 <esso0428@gmail.com>
2023-10-20 20:32:54 +0800

    update venn.lua

--- lua/user/config/plugins/venn.lua
+++ lua/user/config/plugins/venn.lua
@@ -1,8 +1,16 @@
-function _G.Toggle_venn()
+function _G.Toggle_venn(command)
+  if command == "close" then
+    vim.b.venn_enabled = nil
+    vim.cmd [[setlocal ve=NONE]]
+    vim.cmd [[mapclear <buffer>]]
+    print('clear venn and set ve=NONE')
+    return
+  end
   local venn_enabled = vim.inspect(vim.b.venn_enabled)
   if venn_enabled == "nil" then
     vim.b.venn_enabled = true
     vim.cmd [[setlocal ve=all]]
+    print('open venn and set ve=all')
     -- draw a line on HJKL keystokes
     vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>j:VBox<CR>", { noremap = true })
     vim.api.nvim_buf_set_keymap(0, "n", "I", "<C-v>k:VBox<CR>", { noremap = true })
@@ -14,9 +22,11 @@ function _G.Toggle_venn()
     vim.cmd [[setlocal ve=]]
     vim.cmd [[mapclear <buffer>]]
     vim.b.venn_enabled = nil
+    print('mapclear venn and set ve=')
   end
 end
 
 -- toggle keymappings for venn using <leader>v
 -- vim.api.nvim_set_keymap('n', '<leader>tv', ":lua Toggle_venn()<CR>", { noremap = true })
-lvim.keys.normal_mode['<leader>tv'] = "<cmd>lua Toggle_venn()<cr>"
+lvim.keys.normal_mode['<leader>tv'] = "<cmd>lua Toggle_venn('toggle')<cr>"
+lvim.keys.normal_mode['<leader>tc'] = "<cmd>lua Toggle_venn('close')<cr>"
