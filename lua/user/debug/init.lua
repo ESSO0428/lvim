f462b278a94bdff58acdea8087f1dc6b8d5a5d58
Andy6 <esso0428@gmail.com>
2023-10-23 22:43:32 +0800

    update lua/user/debug/init.lua : for dap ui layout

--- lua/user/debug/init.lua
+++ lua/user/debug/init.lua
@@ -1,2 +1,24 @@
 require "user.debug.python"
 require "nvim-dap-virtual-text".setup()
+
+
+lvim.builtin.dap.ui.config.layouts = {
+  {
+    elements = {
+      { id = "scopes",      size = 0.33 },
+      { id = "breakpoints", size = 0.17 },
+      { id = "stacks",      size = 0.25 },
+      { id = "watches",     size = 0.25 },
+    },
+    size = 0.33,
+    position = "left",
+  },
+  {
+    elements = {
+      { id = "repl",    size = 0.45 },
+      { id = "console", size = 0.55 },
+    },
+    size = 0.27,
+    position = "bottom",
+  },
+}
