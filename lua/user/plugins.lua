e08d80d3e36dfa0663ab4043d9f2a9b039e87ff1
Andy6 <esso0428@gmail.com>
2023-12-02 05:38:47 +0800

    fix some bug

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -231,16 +231,10 @@ lvim.plugins = {
   -- },
   {
     "zbirenbaum/copilot.lua",
-    -- event = "VeryLazy",
+    cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
-      -- copilot assume mapped
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_no_tab_map = true
       require("copilot").setup({})
-      -- require("copilot").setup({
-      --   suggestion = { enabled = false },
-      --   panel = { enabled = false },
-      -- })
     end,
   },
   { "f3fora/cmp-spell" },
