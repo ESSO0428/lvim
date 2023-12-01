da9ab96393e88ed4b1bd55d24da9409cae087073
Andy6 <esso0428@gmail.com>
2023-12-02 05:52:55 +0800

    fix settings bug

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -248,7 +248,21 @@ lvim.plugins = {
   {
     "zbirenbaum/copilot-cmp",
     config = function()
-      require("copilot_cmp").setup()
+      require("copilot_cmp").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        }
+      })
     end
   },
   -- { "HiPhish/nvim-ts-rainbow2" },
