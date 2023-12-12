192531666941de68f7d456166ad37ec2dabc170a
ESSO0428 <esso0428@gmail.com>
2023-12-13 03:06:49 +0800

    create UpdateNvimNightlyAndLunaCore.sh (for updating nvim nightly and lunavim core)

--- README.md
+++ README.md
@@ -10,6 +10,22 @@ use neovim pre-config lunavim
 
 ## introduction
 
+### current support neovim version
+
+NVIM v0.9.2 (nightly)
+
+```bash
+$ nvim --version
+NVIM v0.9.2
+Build type: Release
+LuaJIT 2.1.1692716794
+
+   system vimrc file: "$VIM/sysinit.vim"
+  fall-back for $VIM: "/__w/neovim/neovim/build/nvim.AppDir/usr/share/nvim"
+
+Run :checkhealth for more info
+```
+
 ### overview (use this config)
 
 1. install neovim
@@ -51,4 +67,21 @@ some server install neovim will get below error:
 need to use `sudo compile glibc-2.14`  
 if you are only a user, suggest ask `your admin` to help you install `glbc-2.14`
 
+### about update lunavim and neovim core to nightly
+
+1. Can use below command to update lunavim and neovim core to nightly
+    ```bash
+    sh ~/.config/lvim/UpdateNvimNightlyAndLunaCore.sh
+    ```
+2. If update success (can use below command to init lunavim)
+    ```bash
+    # NOTE: init lvim (install plugins and install treesitter parsers)
+    # Maybe restart lvim two times above (because solve plugin dependency)
+    $ lvim .
+    ```
+
+    ```vim
+    " NOTE: Some Python based plugins may need this command to be run after installation.
+    :UpdateRemotePlugins
+    ```
 
---
+++ UpdateNvimNightlyAndLunaCore.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# NOTE: Foolproofing
+echo "This script will update Neovim Nightly and LunaVim core."
+echo "Your current LunaVim configuration will be backed up to ~/.config/lvim_stage/"
+echo "In case of failure, manually restore it by running:"
+echo "mv ~/.config/lvim_stage/ ~/.config/lvim/"
+
+# NOTE: get current neovim version and backup current nvim
+current_nvm_version=`~/nvim.appimage --version | head -2 | paste -sd "_" - | sed -e 's/^NVIM //;s/Build type: //;s/ /_/g'`
+echo "Backup current nvim version: $current_nvm_version"
+echo "mv ~/nvim.appimage ~/nvim.appimage.$current_nvm_version"
+mv ~/nvim.appimage ~/nvim.appimage.$current_nvm_version
+
+# NOTE: Use below command to update nvim nightly (and update lunavim core for nvim nightly)
+cd ~
+mv ~/.config/lvim/ ~/.config/lvim_stage/
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+mv ~/.config/lvim_stage/ ~/.config/lvim/
+
+# NOTE: Install maybe sucessful, now notice below guide
+echo "$ lvim ."
+echo "init lvim (install plugins and install treesitter parsers)"
+echo "Maybe restart lvim two times above (because solve plugin dependency)"
+echo "----------------------------------------"
+echo ":UpdateRemotePlugins"
+echo "Some Python based plugins may need this command to be run after installation."
+
+# NOTE: Ask user whether to remove the old lunarvim directory
+read -p "Do you want to remove the old LunarVim directory at ~/.local/share/lunarvim.old/? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -rf ~/.local/share/lunarvim.old/
+    echo "Old LunarVim directory removed."
+else
+    echo "Old LunarVim directory retained."
+fi
+
