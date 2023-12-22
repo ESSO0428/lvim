3cd281f8812f193ed49c39badeacf472b4e4cc35
Andy6 <esso0428@gmail.com>
2023-12-22 18:47:27 +0800

    update dotenv sql template

--- lua/user/config/plugins/dadbod/dotenv_template/sql.lua
+++ lua/user/config/plugins/dadbod/dotenv_template/sql.lua
@@ -6,6 +6,7 @@ DB_PORT=
 DB_DATABASE=
 DB_USERNAME=
 
+# DB_UI_`DEV` : the `DEV` will becomes the name of the connection (lowercased)
 DB_UI_DEV="${DB_CONNECTION}://${DB_USERNAME}@127.0.0.1:${DB_PORT}/${DB_DATABASE}"
 ]]
   -- 插入模板到当前 buffer
