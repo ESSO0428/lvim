50f8188a44b32be659e4fdec0f9c92e5abb83d72
ESSO0428 <esso0428@gmail.com>
2024-02-08 03:44:56 +0800

    update fold keymappings

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -419,12 +419,18 @@ lvim.plugins = {
     'jghauser/fold-cycle.nvim',
     config = function()
       require('fold-cycle').setup()
-      vim.keymap.set('n', '[<tab>',
+      vim.keymap.set('n', '[f',
         function() return require('fold-cycle').close() end,
         { silent = true, desc = 'Fold-cycle: close folds' })
-      vim.keymap.set('n', ']<tab>',
+      vim.keymap.set('n', ']f',
         function() return require('fold-cycle').open() end,
         { silent = true, desc = 'Fold-cycle: open folds' })
+      vim.keymap.set('n', '[g',
+        function() return require('fold-cycle').close_all() end,
+        { remap = true, silent = true, desc = 'Fold-cycle: close all folds' })
+      vim.keymap.set('n', ']g',
+        function() return require('fold-cycle').open_all() end,
+        { remap = true, silent = true, desc = 'Fold-cycle: Open all folds' })
     end
   },
   {
