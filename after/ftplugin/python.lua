3941d922b35ade65ed13218a44f420110116bf43
ESSO0428 <esso0428@gmail.com>
2024-03-25 01:51:10 +0800

    update after/ftplugin/python.lua

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,4 +1,7 @@
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "ruff_lsp"
+end, lvim.lsp.automatic_configuration.skipped_servers)
 local pyright_opts = {
   single_file_support = true,
   settings = {
