a53c672d4d063b5056b5687dd490165e98dc3c6d
ESSO0428 <esso0428@gmail.com>
2024-03-24 20:55:15 +0800

    update after/ftplugin/python.lua

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,9 +1,4 @@
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- remove `jedi_language_server` from `skipped_servers` list
-lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-  return server ~= "jedi_language_server"
-end, lvim.lsp.automatic_configuration.skipped_servers)
-
 local pyright_opts = {
   single_file_support = true,
   settings = {
