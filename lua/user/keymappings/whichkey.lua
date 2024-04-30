82985752e9bbfb193e1ef81e6b725cabde87c8c7
Andy6 <esso0428@gmail.com>
2024-05-01 05:53:23 +0800

    update whichkey.lua Add CopilotChat functionality to key mappings

--- lua/user/keymappings/whichkey.lua
+++ lua/user/keymappings/whichkey.lua
@@ -76,6 +76,22 @@ lvim.builtin.which_key.vmappings.u = lvim.builtin.which_key.vmappings.l
 lvim.builtin.which_key.vmappings.l = nil
 lvim.builtin.which_key.vmappings.u.a = { "<cmd>Lspsaga code_action<cr>", "Code Action" }
 
+function CopilotChatQuickchat()
+  local input = vim.fn.input("Quick Chat: ")
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+  end
+end
+
+function CopilotChatPromptAction()
+  local actions = require("CopilotChat.actions")
+  require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end
+
+lvim.builtin.which_key.mappings.u['ki'] = { "<cmd>lua CopilotChatQuickchat()<cr>", "CopilotChat - Quick chat" }
+lvim.builtin.which_key.mappings.u['kk'] = { "<cmd>lua CopilotChatPromptAction()<cr>", "CopilotChat Prompt Action" }
+lvim.builtin.which_key.vmappings.u['kk'] = { "<cmd>lua CopilotChatPromptAction()<cr>", "CopilotChat Prompt Action" }
+
 lvim.builtin.which_key.mappings.U = lvim.builtin.which_key.mappings.L
 lvim.builtin.which_key.mappings.L = nil
 
