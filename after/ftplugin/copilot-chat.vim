b912497dd282c0de1d06c1a87c502b2cf669f1e0
Andy6 <esso0428@gmail.com>
2024-05-04 18:06:36 +0800

    update copilot-chat config

---
+++ after/ftplugin/copilot-chat.vim
@@ -0,0 +1,6 @@
+
+
+inoremap <buffer> ,, <++>
+noremap <buffer><LEADER><LEADER> <Esc>/<++><CR>:nohlsearch<CR>"_c4l
+inoremap <buffer> ,c ```<++>```<CR><CR><++><Esc>2ki<CR><Esc>f>a<CR><Esc>2k$a
+inoremap <buffer> ,f <Esc>/<++><CR>:nohlsearch<CR>"_c4l
--- lua/user/keymappings/whichkey.lua
+++ lua/user/keymappings/whichkey.lua
@@ -76,23 +76,6 @@ lvim.builtin.which_key.vmappings.u = lvim.builtin.which_key.vmappings.l
 lvim.builtin.which_key.vmappings.l = nil
 lvim.builtin.which_key.vmappings.u.a = { "<cmd>Lspsaga code_action<cr>", "Code Action" }
 
-function CopilotChatQuickchat()
-  -- local input = vim.fn.input("Quick Chat: ")
-  -- if input ~= "" then
-  --   require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-  -- end
-  require("CopilotChat").ask("", { selection = require("CopilotChat.select").buffer })
-end
-
-function CopilotChatPromptAction()
-  local actions = require("CopilotChat.actions")
-  require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-end
-
-lvim.builtin.which_key.mappings.u['ki'] = { "<cmd>lua CopilotChatQuickchat()<cr>", "CopilotChat - Quick chat" }
-lvim.builtin.which_key.mappings.u['kk'] = { "<cmd>lua CopilotChatPromptAction()<cr>", "CopilotChat Prompt Action" }
-lvim.builtin.which_key.vmappings.u['kk'] = { ":lua CopilotChatPromptAction()<cr>", "CopilotChat Prompt Action" }
-
 lvim.builtin.which_key.mappings.U = lvim.builtin.which_key.mappings.L
 lvim.builtin.which_key.mappings.L = nil
 
