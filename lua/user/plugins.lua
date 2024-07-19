0aef6b2bb87f824f906d0ef965483f3e822efd20
Andy6 <esso0428@gmail.com>
2024-07-19 12:37:23 +0800

    feat(telescope): customize display content
    Extend the telescope configuration with a new table for customizing
    content. This includes a function for creating a table with the values
    to be displayed and a function for creating a function to be used in
    the `display` of an entry.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -884,11 +884,22 @@ lvim.plugins = {
           algorithm = "patience",
           ignore_whitespace = true,
         },
-        telescope = {
-          initial_mode = "normal",
-          layout_strategy = 'horizontal',
-          layout_config = { width = 0.5 },
-        }
+        telescope = vim.tbl_extend(
+          "force",
+          -- telescope theme: https://github.com/nvim-telescope/telescope.nvim#themes
+          require("telescope.themes").get_dropdown({ initial_mode = "normal" }),
+          -- a table for customizing content
+          {
+            -- a function to make a table containing the values to be displayed.
+            -- fun(action: Action): { title: string, client_name: string|nil }
+            make_value = nil,
+
+            -- a function to make a function to be used in `display` of a entry.
+            -- see also `:h telescope.make_entry` and `:h telescope.pickers.entry_display`.
+            -- fun(values: { index: integer, action: Action, title: string, client_name: string }[]): function
+            make_make_display = nil,
+          }
+        ),
       }
     end
   },
