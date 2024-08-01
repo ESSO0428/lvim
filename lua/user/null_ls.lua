ab27f057ef9eacc2583a2c4c7cdd2e84f8af916a
Andy6 <Andy6@www.aimia.nchu.edu.tw>
2024-08-01 22:49:57 +0800

    feat: add null-ls integration for Tailwind CSS
    - Support Tailwind CSS in various filetypes including HTML, Django
      templates, PHP, CSS, and React.
    - Implement Tailwind CSS classes sorting and folding actions.
    - Utilize null-ls to register custom code actions for Tailwind CSS.

---
+++ lua/user/null_ls.lua
@@ -0,0 +1,47 @@
+local null_ls = require("null-ls")
+
+-- define support filetypes for TailwindCSS
+local tailwind_support_filetypes = {
+  "html",
+  "htmldjango",
+  "php",
+  "css",
+  "javascriptreact",
+  "typescriptreact",
+}
+-- define code action for TailwindSort
+local tailwind_code_actions = {
+  {
+    name = "tailwindcss_sort",
+    title = "Sort Tailwind CSS classes",
+    action = function()
+      vim.api.nvim_command("TailwindSort")
+    end,
+  },
+  {
+    name = "tailwind_conceal_toggle",
+    title = "Toggle Tailwind CSS Classes Folding",
+    action = function()
+      vim.api.nvim_command("TailwindConcealToggle")
+    end,
+  },
+}
+-- Use a for loop to register all code actions
+for _, action in ipairs(tailwind_code_actions) do
+  null_ls.register({
+    name = action.name,
+    method = null_ls.methods.CODE_ACTION,
+    filetypes = tailwind_support_filetypes,
+    generator = {
+      fn = function(params)
+        return {
+          {
+            title = action.title,
+            action = action.action,
+          },
+        }
+      end,
+    },
+  })
+end
+null_ls.setup()
