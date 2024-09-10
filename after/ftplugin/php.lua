6664ea11cae9089ee68fc9363d5dce2d874df2c6
ESSO0428 <esso0428@gmail.com>
2024-09-10 18:34:25 +0800

    feat(php): add intelephense LSP configuration
    Added configuration for the intelephense language server to support PHP files.
    This includes setting up the root directory detection and capabilities.

---
+++ after/ftplugin/php.lua
@@ -0,0 +1,18 @@
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "intelephense" })
+local opts = {
+  single_file_support = true,
+  filetypes = { "php" },
+  root_dir = function(...)
+    local util = require "lspconfig.util"
+    return util.find_git_ancestor(...)
+        or util.root_pattern(unpack({
+          "composer.json",
+          ".git",
+          "index.php",
+          "requirements.txt",
+        }))(...)
+  end,
+  capabilities = Nvim.builtin.lsp.capabilities,
+}
+
+require("lvim.lsp.manager").setup("intelephense", opts)
