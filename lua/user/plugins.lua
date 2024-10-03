9210011ca321727047497a2b7ef5f47294703d38
Andy6 <esso0428@gmail.com>
2024-10-04 02:01:17 +0800

    feat: remove pymple.nvim build option from plugins.lua
    - Removed the build option for pymple.nvim in plugins.lua due to activation errors.
    - If dependencies are missing, pymple.nvim will automatically prompt with a warning and installation instructions.
    - Configured pymple.nvim entirely within plugins.lua to ensure settings are enforced and not overridden by default settings.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -333,6 +333,7 @@ lvim.plugins = {
     ft = "python",
     build = ":UpdateRemotePlugins",
     init = function()
+      --#region semshi colorscheme settings
       -- Disabled these features better provided by LSP or other more general plugins
       vim.g["semshi#error_sign"] = false
       vim.g["semshi#simplify_markup"] = false
@@ -364,6 +365,7 @@ lvim.plugins = {
             ]])
         end,
       })
+      --#endregion
     end,
   },
   {
@@ -977,7 +979,67 @@ lvim.plugins = {
       "stevearc/dressing.nvim",
       "nvim-tree/nvim-web-devicons",
     },
-    build = ":PympleBuild"
+    -- NOTE: don't use build here, have some bug
+    -- use pcall require pymple, when dependency not install,
+    -- pymple will jump to `:PympleBuild` warning
+    -- just execute `:PympleBuild` to install dependency
+    config = function()
+      pcall(function()
+        --#region require("pymple").setup({})
+        require("pymple").setup({
+          -- automatically register the following keymaps on plugin setup
+          keymaps = {
+            -- Resolves import for symbol under cursor.
+            -- This will automatically find and add the corresponding import to
+            -- the top of the file (below any existing doctsring)
+            resolve_import_under_cursor = {
+              desc = "Resolve import under cursor",
+              keys = "<leader>uL" -- feel free to change this to whatever you like
+            }
+          },
+          -- logging options
+          logging = {
+            -- whether to log to the neovim console (only use this for debugging
+            -- as it might quickly ruin your neovim experience)
+            console = {
+              enabled = false
+            },
+            -- whether or not to log to a file (default location is nvim's
+            -- stdpath("data")/pymple.vlog which will typically be at
+            -- `~/.local/share/nvim/pymple.vlog` on unix systems)
+            file = {
+              enabled = true,
+              -- the maximum number of lines to keep in the log file (pymple will
+              -- automatically manage this for you so you don't have to worry about
+              -- the log file getting too big)
+              max_lines = 1000,
+              path = vim.fn.stdpath("data") .. "/pymple.vlog",
+            },
+            -- the log level to use
+            -- (one of "trace", "debug", "info", "warn", "error", "fatal")
+            level = "info"
+          },
+          -- python options
+          python = {
+            -- the names of root markers to look out for when discovering a project
+            root_markers = {
+              "pyproject.toml",
+              "setup.py",
+              ".git",
+              "manage.py",
+              "requirements.txt",
+              "setup.cfg",
+              "Pipfile",
+              "pyrightconfig.json",
+            },
+            -- the names of virtual environment folders to look out for when
+            -- discovering a project
+            virtual_env_names = { ".venv" }
+          }
+        })
+        --#endregion
+      end)
+    end,
   },
   {
     "aca/emmet-ls",
