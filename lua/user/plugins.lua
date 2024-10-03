819c7d038d07c8a6480c750cc25830f252645a31
Andy6 <esso0428@gmail.com>
2024-10-03 21:53:13 +0800

    feat(plugins): add pymple.nvim for Python support
    - Added pymple.nvim plugin to enhance Python development experience.
    - Moved configuration to lua/user/config/plugins/pymple.lua for better management.
      - Changed the default keymap for resolve_import_under_cursor from <leader>ll to <leader>uL.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -969,6 +969,17 @@ lvim.plugins = {
   },
   { "stevanmilic/nvim-lspimport" },
   {
+    "alexpasmantier/pymple.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      -- optional (nicer ui)
+      "stevearc/dressing.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    build = ":PympleBuild"
+  },
+  {
     "aca/emmet-ls",
     config = function()
       local lspconfig = require("lspconfig")
