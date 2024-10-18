de27a427785392b1adc1896591bd57cebd14af9b
ESSO0428 <esso0428@gmail.com>
2024-10-18 22:27:42 +0800

    feat: add Narrowing function for normal and visual modes
    - Implemented a custom function to handle narrowing effect outside the
      selected area in visual mode.
    - In normal mode, the function automatically provides narrowing for the
      selected code fold.
    - Bound the function to the visual mode shortcut <leader>On.
    - Updated the plugin initialization to include the Narrowing function.

---
+++ lua/user/config/plugins/Narrowing.lua
@@ -0,0 +1,48 @@
+-- Custom function to implement the Narrow effect outside the selected area
+function narrow_except_selection(visual_mode)
+  -- 保存當前視窗位置
+  local start_line, end_line
+  if visual_mode then
+    -- Get the currently selected area (visual mode)
+    _, start_line, _, _ = unpack(vim.fn.getpos("'<"))
+    _, end_line, _, _ = unpack(vim.fn.getpos("'>"))
+  else
+    vim.cmd('normal! zR')
+    local ok, err = pcall(function()
+      vim.cmd('normal! zo')
+    end)
+    if not ok then
+      print("Not Found Fold")
+      return
+    end
+    start_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+
+    vim.cmd('normal! ]z')
+    end_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  end
+  vim.fn.cursor(start_line, 0)
+
+  vim.cmd('split')
+  vim.cmd('UfoDisable')
+
+  pcall(function() vim.cmd('normal! zo') end)
+
+  -- Handle the part above the selection
+  if start_line > 1 then
+    -- Move to the first line, select to the line before the start line, unfold and fold
+    vim.fn.cursor(start_line - 1, 0)
+    pcall(function() vim.cmd('normal! Vggzdgvzf') end)
+  end
+
+  -- Handle the part below the selection
+  local last_line = vim.fn.line('$')
+  if end_line < last_line then
+    -- Move to the line after the end line, select to the last line, unfold and fold
+    vim.fn.cursor(end_line + 1, 0)
+    pcall(function() vim.cmd('normal! VGzdgvzf') end)
+  end
+end
+
+-- Bind the function to the shortcut key <leader>On in visual mode
+lvim.keys.visual_mode['<leader>On'] = ':<C-u>lua narrow_except_selection(true)<CR>'
+lvim.builtin.which_key.mappings['On'] = { '<cmd>lua narrow_except_selection()<CR>', 'Zoom-in Folding to split' }
