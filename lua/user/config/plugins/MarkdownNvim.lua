548ef0be7426d13045db50c28960a3c99ea071e8
ESSO0428 <esso0428@gmail.com>
2024-10-23 15:54:47 +0800

    feat(plugins): remove MarkdownNvim icon config
    This change includes the deletion of the MarkdownNvim.lua file and the
    removal of its reference in the init.lua file.

--- lua/user/config/plugins/MarkdownNvim.lua
+++
@@ -1,8 +0,0 @@
-local has_devicons, devicons = pcall(require, 'nvim-web-devicons')
-require("render-markdown.core.icons").get = function(language)
-  if has_devicons then
-    return devicons.get_icon_by_filetype(language)
-  else
-    return nil, nil
-  end
-end
