039462d07ae8c338fb483ede83e25b8ed4f117cd
ESSO0428 <esso0428@gmail.com>
2024-11-21 04:42:39 +0800

    feat(plugins): add link options in render-markdown config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1187,6 +1187,30 @@ lvim.plugins = {
             rendered = 2,
           },
         },
+        link = {
+          -- Turn on / off inline link icon rendering
+          enabled = true,
+          -- Inlined with 'image' elements
+          image = '󰥶 ',
+          -- Inlined with 'email_autolink' elements
+          email = '󰀓 ',
+          -- Fallback icon for 'inline_link' elements
+          hyperlink = '󰌹 ',
+          -- Applies to the fallback inlined icon
+          highlight = 'RenderMarkdownLink',
+          -- Applies to WikiLink elements
+          wiki = { icon = '󱗖 ', highlight = 'RenderMarkdownWikiLink' },
+          -- Define custom destination patterns so icons can quickly inform you of what a link
+          -- contains. Applies to 'inline_link' and wikilink nodes.
+          -- Can specify as many additional values as you like following the 'web' pattern below
+          --   The key in this case 'web' is for healthcheck and to allow users to change its values
+          --   'pattern':   Matched against the destination text see :h lua-pattern
+          --   'icon':      Gets inlined before the link text
+          --   'highlight': Highlight for the 'icon'
+          custom = {
+            web = { pattern = '^http[s]?://', icon = '󰖟 ', highlight = 'RenderMarkdownLink' },
+          },
+        },
         callout = {
           note = { raw = '[!NOTE]', rendered = '󰋽 Note', highlight = 'RenderMarkdownInfo' },
           tip = { raw = '[!TIP]', rendered = '󰌶 Tip', highlight = 'RenderMarkdownSuccess' },
