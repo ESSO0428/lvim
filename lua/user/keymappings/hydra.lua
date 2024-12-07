a8db9c536bced86aa460b505ea01e6fa3bdc634e
ESSO0428 <esso0428@gmail.com>
2024-12-08 00:52:36 +0800

    refactor(keymap of hydra): partially standardize hydra menu bindings
    Standardize most navigation keys in hydra menus to use `<` and `>`,
    while keeping some `(` and `)` bindings to differentiate certain functions.
    This maintains consistency while preserving functional distinctions.
    
    - Change most debug breakpoint keys
    - Add bookmarks hydra with mixed bindings
    - Keep `(` and `)` for cnext and cprevious of quickfix jumps

--- lua/user/keymappings/hydra.lua
+++ lua/user/keymappings/hydra.lua
@@ -1,10 +1,11 @@
 local got_hydra, hydra = pcall(require, "hydra")
 lvim.builtin.which_key.setup.plugins.presets.h = false
-local hint
-hint = [[
+
+local hint = [[
 _(_/_)_: c previous/next
 ]]
-local hydra_config = {
+
+hydra({
   name = "quickfix",
   mode = { "n" },
   hint = hint,
@@ -18,13 +19,13 @@ local hydra_config = {
     { ')', ':cnext<cr>',     { desc = 'cnext' } },
     { '(', ':cprevious<CR>', { desc = 'cprevious' } }
   }
-}
-hydra(hydra_config)
+})
 
 hint = [[
-_[_/_]_: breakpint prev/next _S_: go to breakpint stop
+_<_/_>_: breakpint prev/next _S_: go to breakpint stop
 ]]
-hydra_config = {
+
+hydra({
   name = "debug",
   mode = { "n" },
   hint = hint,
@@ -35,17 +36,17 @@ hydra_config = {
   },
   body = "<leader>hd",
   heads = {
-    { ']', ":lua require('goto-breakpoints').next()<CR>",    { desc = 'next breakpoints' } },
-    { '[', ":lua require('goto-breakpoints').prev()<CR>",    { desc = 'prev breakpoints' } },
+    { '<', ":lua require('goto-breakpoints').next()<CR>",    { desc = 'next breakpoints' } },
+    { '>', ":lua require('goto-breakpoints').prev()<CR>",    { desc = 'prev breakpoints' } },
     { 'S', ":lua require('goto-breakpoints').stopped()<CR>", { desc = 'go to breakpoints stop' } },
   }
-}
-hydra(hydra_config)
+})
 
 hint = [[
 _o_: Folding Code (Toggle) _u_: Folding Preview
 ]]
-hydra_config = {
+
+hydra({
   name = "FoldMode",
   mode = { "n" },
   hint = hint,
@@ -59,14 +60,33 @@ hydra_config = {
     { 'o', "za",                                    { desc = 'Folding Code (Toggle)' } },
     { 'u', ":lua peekFoldedLinesUnderCursor()<cr>", { desc = "Folding Preview" } }
   }
-}
-
-hydra(hydra_config)
+})
 
 hint = [[
-_[_/_]_: spell prev/next
+_<_/_>_: bookmarks prev/next
 ]]
-hydra_config = {
+
+hydra({
+  name = "bookmarks",
+  mode = { "n" },
+  hint = hint,
+  config = {
+    invoke_on_body = true,
+    color = "pink",
+    hint = { border = "rounded" },
+  },
+  body = "<leader>hm",
+  heads = {
+    { '>', ":lua require('bookmarks.actions').bookmark_next()<cr>", { desc = 'next bookmark' } },
+    { '<', ":lua require('bookmarks.actions').bookmark_prev()<cr>", { desc = 'prev bookmark' } },
+  }
+})
+
+hint = [[
+_<_/_>_: spell prev/next
+]]
+
+hydra({
   name = "spell",
   mode = { "n" },
   hint = hint,
@@ -75,10 +95,9 @@ hydra_config = {
     color = "pink",
     hint = { border = "rounded" },
   },
-  body = "sp",
+  body = "<leader>hs",
   heads = {
-    { ']', "]s",    { desc = 'next spell' } },
-    { '[', "[s",    { desc = 'prev spell' } },
+    { '>', "]s", { desc = 'next spell' } },
+    { '<', "[s", { desc = 'prev spell' } },
   }
-}
-hydra(hydra_config)
+})
