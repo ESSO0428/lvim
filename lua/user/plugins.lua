414eaddd457fdcde2afd64de72dcc3ccb99c2a1a
ESSO0428 <esso0428@gmail.com>
2024-12-08 17:36:32 +0800

    refactor(theme): adjust python semantic tokens highlighting
    - Disable highlight color for imported symbols in semshi
    - Modify LSP semantic tokens for Python decorators and classes
    - Add readonly modifier highlight for Python constants

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -397,7 +397,8 @@ lvim.plugins = {
           -- vim.cmd "au ColorScheme * highlight! link semshiBuiltin @function.builtin"
           -- vim.cmd "au ColorScheme * highlight! link semshiBuiltin @field"
           vim.cmd([[
-            highlight! semshiImported gui=bold guifg=#4ec9b0
+            " highlight! semshiImported gui=bold guifg=#4ec9b0
+            highlight! semshiImported gui=bold guifg=none
             " highlight! semshiGlobal gui=bold
             " highlight! semshiImported guifg=#4ec9b0
             highlight! link semshiGlobal cleared
