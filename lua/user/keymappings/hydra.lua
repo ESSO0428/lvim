dd1ee5ad65e202d23ab687b2b82bf5bf8f122644
ESSO0428 <esso0428@gmail.com>
2025-04-28 21:02:59 +0800

    feat(hydra): add big file scroll navigation
    Add a new Hydra for navigating large files by scrolling in
    1% increments. This hydra helps with performance when dealing
    with very large files, making navigation more efficient by
    using proportional jumps instead of fixed line counts.

--- lua/user/keymappings/hydra.lua
+++ lua/user/keymappings/hydra.lua
@@ -101,3 +101,23 @@ hydra({
     { '<', "[s", { desc = 'prev spell' } },
   }
 })
+
+hint = [[
+_<c-u>_/_<c-o>_: scroll 1% up/down
+]]
+
+hydra({
+  name = "bigfile scroll",
+  mode = { "n" },
+  hint = hint,
+  config = {
+    invoke_on_body = true,
+    color = "pink",
+    hint = { float_opts = {} },
+  },
+  body = "<leader>hb",
+  heads = {
+    { '<c-o>', [[<cmd>exe "normal! " . (line('$') >= 100 ? float2nr(ceil(line('$') / 100)) : 1) . "j"<cr>]], { desc = 'scroll 1% down' } },
+    { '<c-u>', [[<cmd>exe "normal! " . (line('$') >= 100 ? float2nr(ceil(line('$') / 100)) : 1) . "k"<cr>]], { desc = 'scroll 1% up' } },
+  }
+})
