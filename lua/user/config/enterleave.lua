41ed7458274fadc7875aa5b3a44e36b7910fdd54
ESSO0428 <esso0428@gmail.com>
2026-02-09 23:59:53 +0800

    perf: disable auto-open file explorer to improve startup time
    Comment out nvim-tree and neo-tree auto-open logic on VimEnter.
    Auto-open is now only supported when restoring sessions.

--- lua/user/config/enterleave.lua
+++ lua/user/config/enterleave.lua
@@ -1,4 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
+--[[  WARNING: Below will open nvim-tree or neo-tree when nvim is argument opened files. But affects startup speed.
 if vim.fn.argc() == 0 or vim.fn.argv(0) ~= "." then
   if lvim.builtin.nvimtree.active == false then
     autocmd({ "VimEnter" }, { callback = open_neo_tree })
@@ -6,4 +7,5 @@ if vim.fn.argc() == 0 or vim.fn.argv(0) ~= "." then
     autocmd({ "VimEnter" }, { callback = open_nvim_tree })
   end
 end
+--]]
 autocmd({ "VimLeave" }, { pattern = "*", command = "set guicursor= | call chansend(v:stderr, \"\x1b[5 q\")" })
