d309a57e08bf1428bc6521b98521f412b23b2ebc
ESSO0428 <esso0428@gmail.com>
2026-02-13 13:19:32 +0800

    refactor: replace hardcoded config paths with vim.fn.stdpath

--- lua/user/snippets/other.lua
+++ lua/user/snippets/other.lua
@@ -1,5 +1,5 @@
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = {
-    "~/.config/lvim/snippets/other/rpinski.shebang-snippets-0.1.4"
+    vim.fn.stdpath('config') .. "/snippets/other/rpinski.shebang-snippets-0.1.4"
   }
 })
--- lua/user/snippets/web.lua
+++ lua/user/snippets/web.lua
@@ -1,10 +1,10 @@
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = {
-    "~/.config/lvim/snippets/bootstrap5/anbuselvanrocky.bootstrap5-vscode-0.4.2"
+    vim.fn.stdpath('config') .. "/snippets/bootstrap5/anbuselvanrocky.bootstrap5-vscode-0.4.2"
   }
 })
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = {
-    "~/.config/lvim/snippets/django/bigonesystems.django-1.0.2"
+    vim.fn.stdpath('config') .. "/snippets/django/bigonesystems.django-1.0.2"
   }
 })
